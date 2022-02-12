39b3ec03e3e18e29221d06adca817ecceebfe4b3
Mahnoor Qureshi <2454285q@student.gla.ac.uk>
2022-02-12 14:15:42 +0000

    Added a bullets.cs file (for real this time)

---
+++ src/bullets.cs
@@ -0,0 +1,16 @@
+pusing System;
+ublic class WordBullets {
+
+    public WordBullets(gameChip gameChip) {
+
+    }
+
+    public void Update(GameChip gameChip) {
+
+    }
+
+    public void Draw(GameChip gameChip) {
+
+        
+    }
+}
